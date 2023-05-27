1ccf26697d521c095dd4ef368dd0570c42a93a13
Ugwu Obed <vjazzyvictor@gmail.com>
2023-05-27 19:44:51 +0100

    fest

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import styles from '../styles/dashboard.module.css';
+import dashboardStyles from '../styles/dashboard.module.css';
 import Image from 'next/image';
+import globalStyles from '../styles/styles.css';
 
 const DashboardPage = () => {
   return (
-    <div className={styles['dashboard-container']}>
-      <div className={styles['logo-container']}>
-        {/* Add your logo image or logo component here */}
-        <Image src="/zikor-logo.png" alt="Logo" width={200} height={100} />
+    <div className={dashboardStyles['dashboard-container']}>
+      <div className={dashboardStyles['gray-background']}>
+        <div className={dashboardStyles['left-side']}>
+          <div className={dashboardStyles['logo-container']}>
+            <Image src="/zikor-logo.png" alt="Logo" width={100} height={50} />
+            <div className={dashboardStyles['icons-container']}>
+              <ul>
+                <li><a href="#"><Image src="/home.png" alt="Home" width={25} height={25} /> Home</a></li>
+                <li><a href="#"><Image src="/noti.png" alt="Logo" width={25} height={25} /> Notification</a></li>
+                <li><a href="#"><Image src="/setting.png" alt="Logo" width={25} height={25} /> Settings</a></li>
+                <li><a href="#"><Image src="/sms.png" alt="Logo" width={25} height={25} /> Help</a></li>
+                <li><a href="#"><Image src="/call.png" alt="Logo" width={25} height={25} /> Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={styles['user-container']}>
-        <span>Welcome</span>
+      <div className={dashboardStyles['right-side']}>
+        <div className={dashboardStyles['user-container']}>
+          <span>Welcome</span>
+        </div>
       </div>
       {/* Add the rest of your dashboard content here */}
     </div>
@@ -18,5 +33,3 @@ const DashboardPage = () => {
 };
 
 export default DashboardPage;
-
-
