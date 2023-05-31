a3c1bd0b7fac35b8e6e066628973a42b7a22257f
Ugwu Obed <vjazzyvictor@gmail.com>
2023-05-31 14:05:37 +0100

    fest

--- pages/product.js
+++ pages/product.js
@@ -1,9 +1,118 @@
-import React from 'react';
+import React, { useState } from 'react';
 import dashboardStyles from '../styles/product.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const DashboardPage = () => {
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [showAddProduct, setShowAddProduct] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [formValues, setFormValues] = useState({});
+  const [isProductAdded, setIsProductAdded] = useState(false);
+
+  const handleMoreClick = (rowIndex) => {
+    setSelectedRow(rowIndex === selectedRow ? null : rowIndex);
+  };
+
+  const handleAddButtonClick = () => {
+    setShowAddProduct(true);
+  };
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const handleInputChange = (e) => {
+    setFormValues({ ...formValues, [e.target.id]: e.target.value });
+  };
+
+  const handleUpload = () => {
+    // Perform upload logic here
+    // Once upload is successful, set isProductAdded to true to display the notification
+    setIsProductAdded(true);
+    // Reset form values
+    setFormValues({});
+  };
+
+  const renderForm = () => {
+    if (selectedCategory === 'cloths') {
+      return (
+        <div className={dashboardStyles['form-wrapper']}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" value={formValues.name || ''} onChange={handleInputChange} />
+          </div>
+          <div>
+            <label htmlFor="type">Type:</label>
+            <input type="text" id="type" value={formValues.type || ''} onChange={handleInputChange} />
+          </div>
+          <div>
+            <label htmlFor="brand">Brand:</label>
+            <input type="text" id="brand" value={formValues.brand || ''} onChange={handleInputChange} />
+          </div>
+          <div>
+            <label htmlFor="size">Size:</label>
+            <input type="text" id="size" value={formValues.brand || ''} onChange={handleInputChange} />
+          </div>
+          <div>
+            <label htmlFor="category">Gender:</label>
+            <select id="category" value={formValues.category || ''} onChange={handleInputChange}>
+              <option value="">Select a gender category</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Unisex">Unisex</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="description">Description:</label>
+            <textarea id="description" value={formValues.description || ''} onChange={handleInputChange}></textarea>
+          </div>
+          <div>
+            <label htmlFor="color">Color:</label>
+            <input type="text" id="color" value={formValues.color || ''} onChange={handleInputChange} />
+          </div>
+          <div>
+            <label htmlFor="price">Price:</label>
+            <input type="text" id="price" value={formValues.price || ''} onChange={handleInputChange} />
+          </div>
+          <div>
+            <label htmlFor="image">Image:</label>
+            <input type="file" id="image" accept="image/*" />
+          </div>
+          <div>
+            <button type="button" onClick={handleUpload}>
+              Upload
+            </button>
+          </div>
+        </div>
+      );
+    } else if (selectedCategory === 'food') {
+      return (
+        <div>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" value={formValues.name || ''} onChange={handleInputChange} />
+          </div>
+          {/* Add food specific form fields here */}
+          {/* ... */}
+        </div>
+      );
+    } else if (selectedCategory === 'shoes') {
+      return (
+        <div>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" value={formValues.name || ''} onChange={handleInputChange} />
+          </div>
+          {/* Add shoes specific form fields here */}
+          {/* ... */}
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
+
   return (
     <div className={dashboardStyles['product-container']}>
       <div className={dashboardStyles['gray-background']}>
@@ -12,11 +121,36 @@ const DashboardPage = () => {
             <Image src="/zikor-logo.png" alt="Logo" width={100} height={50} />
             <div className={dashboardStyles['icons-container']}>
               <ul>
-                <li><a href="#"><Image src="/home.png" alt="Home" width={25} height={25} /> <span className={dashboardStyles['nav-text']}>Home</span></a></li>
-                <li><a href="#"><Image src="/noti.png" alt="Notification" width={25} height={25} /> <span className={dashboardStyles['nav-text']}>Notification</span></a></li>
-                <li><a href="#"><Image src="/setting.png" alt="Settings" width={25} height={25} /> <span className={dashboardStyles['nav-text']}>Settings</span></a></li>
-                <li><a href="#"><Image src="/sms.png" alt="Help" width={25} height={25} /> <span className={dashboardStyles['nav-text']}>Help</span></a></li>
-                <li><a href="#"><Image src="/call.png" alt="Contact Us" width={25} height={25} /> <span className={dashboardStyles['nav-text']}>Contact Us</span></a></li>
+                <li>
+                  <a href="#">
+                    <Image src="/home.png" alt="Home" width={25} height={25} />{' '}
+                    <span className={dashboardStyles['nav-text']}>Home</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <Image src="/noti.png" alt="Notification" width={25} height={25} />{' '}
+                    <span className={dashboardStyles['nav-text']}>Notification</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <Image src="/setting.png" alt="Settings" width={25} height={25} />{' '}
+                    <span className={dashboardStyles['nav-text']}>Settings</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <Image src="/sms.png" alt="Help" width={25} height={25} />{' '}
+                    <span className={dashboardStyles['nav-text']}>Help</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <Image src="/call.png" alt="Contact Us" width={25} height={25} />{' '}
+                    <span className={dashboardStyles['nav-text']}>Contact Us</span>
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
@@ -24,57 +158,136 @@ const DashboardPage = () => {
       </div>
       <div className={dashboardStyles['center']}>
         <ul className={dashboardStyles['list-items']}>
-        <li>
+          <li>
             <Link href="/dashboard" as="/dashboard">
               <h4>Dashboard</h4>
             </Link>
           </li>
-          <li className={dashboardStyles['active']}><a href="#">Products</a></li>
-          <li><a href="#">Order</a></li>
-          <li><a href="#">Completed</a></li>
+          <li className={dashboardStyles['active']}>
+            <a href="#">Products</a>
+          </li>
+          <li>
+            <a href="#">Order</a>
+          </li>
+          <li>
+            <a href="#">Completed</a>
+          </li>
         </ul>
       </div>
 
       <div className={dashboardStyles['search-bar']}>
-  <input type="text" placeholder="Search products by name" />
-  <button type="button">Search</button>
-</div>
-<div className={dashboardStyles['button-add']}>
-<button className={dashboardStyles['add-button']} style={{float: "left"}}>Add</button>
-</div>
-<div className={dashboardStyles['table-container']}>
-  <h2 className={dashboardStyles['table-title']}>Products</h2>
-  <table className={dashboardStyles['product-table']}>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Type</th>
-        <th>Price</th>
-        <th>Category</th>
-        <td className={dashboardStyles['last-body']}>More</td>
-      </tr>
-    </thead>
-    <tbody>
-      {/* Dummy table content */}
-      <tr>
-        <td>Product 1</td>
-        <td>Type 1</td>
-        <td>$10</td>
-        <td>Category 1</td>
-        <td className={dashboardStyles['last-body']}>More 1</td>
-      </tr>
-      <tr>
-        <td>Product 2</td>
-        <td>Type 2</td>
-        <td>$20</td>
-        <td>Category 2</td>
-        <td className={dashboardStyles['last-body']}>More 2</td>
-      </tr>
-      {/* ... */}
-    </tbody>
-  </table>
-</div>
-
+        <input type="text" placeholder="Search products by name" />
+        <button type="button">Search</button>
+      </div>
+      <div className={dashboardStyles['button-add']}>
+        <button className={dashboardStyles['add-button']} style={{ float: 'left' }} onClick={handleAddButtonClick}>
+          Add
+        </button>
+      </div>
+      {showAddProduct && (
+        <div className={`${dashboardStyles['add-product-section']} ${dashboardStyles['show-form']}`}>
+          <h2>Add Product</h2>
+          <div className={dashboardStyles['category-container']}>
+            <label htmlFor="category">Category:</label>
+            <select id="category" value={selectedCategory} onChange={handleCategoryChange}>
+              <option value="">Select a option</option>
+              <option value="cloths">Cloths</option>
+              <option value="food">Food</option>
+              <option value="shoes">Shoes</option>
+            </select>
+          </div>
+          {renderForm()}
+        </div>
+      )}
+      {isProductAdded && (
+        <div className={dashboardStyles['notification']}>
+          Product successfully added!
+        </div>
+      )}
+      <div className={dashboardStyles['table-container']}>
+        <h2 className={dashboardStyles['table-title']}>Products</h2>
+        <table className={dashboardStyles['product-table']}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Price</th>
+              <th>Category</th>
+              <td className={dashboardStyles['last-body']}>More</td>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Dummy table content */}
+            <tr>
+              <td>Product 1</td>
+              <td>Type 1</td>
+              <td>$10</td>
+              <td>Category 1</td>
+              <td className={dashboardStyles['last-body']}>
+                <a href="#">
+                  <Image
+                    src="/more.png"
+                    alt="More"
+                    width={20}
+                    height={8}
+                    onClick={() => handleMoreClick(0)}
+                  />
+                </a>
+                {selectedRow === 0 && (
+                  <div className={dashboardStyles['more-options']}>
+                    <ul>
+                      <li>
+                        <a href="#" className={dashboardStyles.edit}>
+                          Edit
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className={dashboardStyles.delete}>
+                          Delete
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>Product 2</td>
+              <td>Type 2</td>
+              <td>$20</td>
+              <td>Category 2</td>
+              <td className={dashboardStyles['last-body']}>
+                <a href="#">
+                  <Image
+                    src="/more.png"
+                    alt="More"
+                    width={20}
+                    height={8}
+                    onClick={() => handleMoreClick(1)}
+                  />
+                </a>
+                {selectedRow === 1 && (
+                  <div className={dashboardStyles['more-options']}>
+                    <ul>
+                      <li>
+                        <a href="#" className={dashboardStyles.edit}>
+                          Edit
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className={dashboardStyles.delete}>
+                          Delete
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </td>
+            </tr>
+            {/* ... */}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
