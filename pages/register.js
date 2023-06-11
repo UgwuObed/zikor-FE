a3f828ae57fc45d9e51375f117f5af87f570394b
Ugwu Obed <vjazzyvictor@gmail.com>
2023-06-11 20:37:46 +0100

    fest

--- pages/register.js
+++ pages/register.js
@@ -2,76 +2,84 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 const RegisterForm = () => {
-const [firstName, setFirstName] = useState('');
-const [lastName, setLastName] = useState('');
-const [businessName, setBusinessName] = useState('');
-const [email, setEmail] = useState('');
-const [phone, setPhone] = useState('');
-const [password, setPassword] = useState('');
-const [passwordConfirmation, setPasswordConfirmation] = useState('');
-const [csrfToken, setCsrfToken] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [businessName, setBusinessName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [csrfToken, setCsrfToken] = useState('');
 
-const router = useRouter();
+  const router = useRouter();
 
-useEffect(() => {
-async function fetchCsrfToken() {
-try {
-const response = await fetch('http://localhost:8000/csrf-cookie');
-if (response.ok) {
-setCsrfToken(response.headers.get('X-CSRF-TOKEN'));
-} else {
-console.log('Failed to fetch CSRF token');
-}
-} catch (error) {
-console.log('An error occurred while fetching CSRF token:', error);
-}
-}
+  useEffect(() => {
+    async function fetchCsrfToken() {
+      try {
+        const response = await fetch('http://localhost:8000/csrf-cookie');
+        if (response.ok) {
+          setCsrfToken(response.headers.get('X-CSRF-TOKEN'));
+        } else {
+          console.log('Failed to fetch CSRF token');
+        }
+      } catch (error) {
+        console.log('An error occurred while fetching CSRF token:', error);
+      }
+    }
 
-fetchCsrfToken();
-}, []);
+    fetchCsrfToken();
+  }, []);
 
-const handleSubmit = async (e) => {
-e.preventDefault();
-if (password !== passwordConfirmation) {
-  console.log('Password and password confirmation do not match');
-  return;
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== passwordConfirmation) {
+      console.log('Password and password confirmation do not match');
+      return;
+    }
 
-const formData = {
-  first_name: firstName,
-  last_name: lastName,
-  business_name: businessName,
-  email,
-  phone,
-  password,
-  password_confirmation: passwordConfirmation,
-};
+    const formData = {
+      first_name: firstName,
+      last_name: lastName,
+      business_name: businessName,
+      email,
+      phone,
+      password,
+      password_confirmation: passwordConfirmation,
+    };
 
-try {
-  const response = await fetch('http://localhost:8000/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-CSRF-TOKEN': csrfToken,
-    },
-    body: JSON.stringify(formData),
-  });
+    try {
+      const response = await fetch('http://localhost:8000/register.store', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-TOKEN': csrfToken,
+        },
+        body: JSON.stringify(formData),
+      });
 
-  if (response.ok) {
-    // Registration successful, redirect to the profile creation page
-    router.push('/profile/create');
-  } else {
-    // Handle error
-    const errorData = await response.json();
-    console.log('Error:', errorData);
-  }
-} catch (error) {
-  // Handle fetch error
-  console.log('An error occurred:', error);
-}
-};
+      if (response.ok) {
+        // Registration successful
+        console.log('Registration successful!');
+        // Optionally, you can clear the form fields here
+        setFirstName('');
+        setLastName('');
+        setBusinessName('');
+        setEmail('');
+        setPhone('');
+        setPassword('');
+        setPasswordConfirmation('');
+      } else {
+        // Handle error
+        const errorData = await response.json();
+        console.log('Error:', errorData);
+      }
+    } catch (error) {
+      // Handle fetch error
+      console.log('An error occurred:', error);
+    }
+  };
 
- return (
+  return (
     <form onSubmit={handleSubmit}>
       <label>
         First Name:
