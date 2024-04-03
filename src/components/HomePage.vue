409a9e90077684b85f0c1c8bb6b499ad14434d41
UgwuObed <vjazzyvictor@gmail.com>
2024-04-03 18:16:47 +0100

    fest

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,32 +1,214 @@
 <template>
-  <div class="hello">
-  <h1>Home</h1>
+    <div class="sidebar">
+       <div class="hamburger-menu" @click="toggleMenu">
+        <p v-if="isMobile">☰</p>
+      </div>
+    </div>
+  <div class="home-page">
+  
+    <!-- Main content area -->
+    <div class="content">
+      <div class="layer1">
+        <div class="rectangle-content">
+          <h4>Wallet Balance</h4>
+          <h3>NGN 120,00.00</h3>
+          <p>Improve sales by regularly updating your products</p>
+          <button @click="upload">Add Product</button>
+
+          <div class="inner-rectangle">
+            <span>Products</span>
+            <span>Total Sales</span>
+            <span>Order</span>
+            <div>1</div>
+            <div>0</div>
+            <div>0</div>
+          </div>
+        </div>
+      </div> 
+    </div>
   </div>
+  <h1>Sales Overview</h1>
+      <div class="chart-rectangle">
+      <canvas id="salesChart"></canvas>
+      </div>
 </template>
 
 <script>
+import Chart from 'chart.js/auto';
+
 export default {
-  name: 'HomePage',
-  props: {
-    msg: String
+  data() {
+    return {
+      isMobile: false, 
+      showMenu: false 
+    };
+  },
+  mounted() {
+    this.renderChart();
+    const width = window.innerWidth;
+    this.isMobile = width <= 600;
+  },
+  methods: {
+
+  toggleMenu() {
+      this.showMenu = !this.showMenu;
+    },
+    renderChart() {
+      const ctx = document.getElementById('salesChart').getContext('2d');
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+          datasets: [{
+            label: 'Monthly Sales',
+            data: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160],
+            backgroundColor: '#5E17EB',
+            borderWidth: 1,
+          }]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    },
+    upload() {
+      // Add functionality for upload button here
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.hamburger-menu {
+  display: none; 
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+@media only screen and (max-width: 600px) {
+  .home-page {
+    display: flex;
+    height: 50vh;
+    width: 90%;
+    margin-left: 23px;
+    overflow-x: hidden;
+    font-family: 'Poppins';
+    border: 1px solid #5E17EB;
+    border-radius: 5px;
+    outline: none;
+    position: relative; 
+  }
+
+  .hamburger-menu {
+    display: block; 
+    margin-right: 385px;
+    margin-bottom: 20px;
+    margin-top: -30px;
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  .content {
+    flex: 1;
+    position: relative;
+  }
+
+  .layer1 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(94, 23, 235, 0.31);
+  }
+
+  .rectangle-content {
+    position: absolute;
+    top: 30%;
+    left: 40%; 
+    transform: translate(-50%, -50%);
+    text-align: left;
+  }
+
+  .rectangle-content h4 {
+    font-size: 16px;
+    font-weight: normal;
+    margin-bottom: 5px; 
+  }
+
+  .rectangle-content h3 {
+    font-size: 23px; 
+    margin-top: 5px; 
+  }
+
+  .rectangle-content p {
+    font-size: 16px; 
+  }
+
+  .rectangle-content button {
+    position: absolute;
+    top: 30px; 
+    right: -100px; 
+    background-color: #5E17EB;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .inner-rectangle {
+    position: absolute;
+    top: 100%;
+    left: -10px;
+    width: 145%;
+    height: 65%;
+    text-align: center;
+    background-color: #5E17EB;
+    border-radius: 5px;
+    color: white;
+  }
+
+  .inner-rectangle span {
+    display: inline-block;
+    width: 33.33%;
+    margin-top: 10%;
+  }
+
+  .inner-rectangle div {
+    display: inline-block;
+    width: 33.33%;
+  }
+
+h1 {
+    font-size: 20px;
+    font-weight: normal;
+    font-family: 'Poppins';
+    margin-right: 250px;
+    color: black;
+   }
+ 
+  .chart-rectangle {
+    position: absolute;
+    top: 69%;
+    left: 5%;
+    width: 90%;
+    height: 280px; 
+    background-color: rgba(94, 23, 235, 0.31);
+    border-radius: 5px;
+  }
+
+  
+  #salesChart {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
+
+
